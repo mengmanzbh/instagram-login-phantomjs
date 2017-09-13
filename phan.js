7ccb4106e653f93b8e91cb25c8a9e2a3c5fa36cb
mengmanzbh <1361006944@qq.com>
2017-09-13 11:00:02 +0800

    Update phan.js

--- phan.js
+++ phan.js
@@ -2,9 +2,8 @@ var page = require('webpage').create();
 page.viewportSize = { width:1024 , height: 600 };
 page.settings.userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36';
 
-
-page.open('https:/instagram.com/accounts/login/', function() {
-	 
+page.open('https:/instagram.com/accounts/login/', function() {	
+	
 	var ig = page.evaluate(function() {
 		function getCoords(box) {
 			return  {
@@ -26,10 +25,6 @@ page.open('https:/instagram.com/accounts/login/', function() {
 			pass: getPosition('input', 'password'),
 			login: getPosition('button')
 		};
-	 				
-
-	 				
-	 			
 	 });
 	
 	 // fill in data and press login
@@ -38,13 +33,11 @@ page.open('https:/instagram.com/accounts/login/', function() {
 
 	 page.sendEvent('click',ig.pass.x, ig.pass.y);
 	 page.sendEvent('keypress', 'mypassword');
-     page.sendEvent('click',ig.login.x, ig.login.y);
-
+         page.sendEvent('click',ig.login.x, ig.login.y);
 
 	// wait for response
 	setTimeout(function() {
 		page.render('insta.png');
 		phantom.exit();
 	}, 5000);
-	
 });
